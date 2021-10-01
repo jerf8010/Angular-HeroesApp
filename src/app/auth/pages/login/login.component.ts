b0e064ef9062c19698fb9b07330731f40860df17
Emanuel <jerf8010@gmail.com>
2021-10-01 13:58:41 -0500

    HeroesApp finalizada

---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  constructor( private router: Router,
+              private authService: AuthService ) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    // Ir al backend
+
+    // un usuario
+
+    this.authService.login()
+      .subscribe( resp => {
+        if( resp.id ){
+          this.router.navigate(['./heroes'])
+        }
+      })
+    
+
+  }
+
+}
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+import { Auth } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth(): Auth {
+    return {...this._auth! }
+  }
+  
+  constructor( private http: HttpClient ) { }
+
+  verificaAutentificacion(): Observable<boolean> {
+    if( !localStorage.getItem('id') ){
+      //return of(false); // transforma false en observable
+      return of(false);
+    }
+
+    
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+              .pipe(
+                map( auth =>  {
+                 this._auth = auth;
+                  return true;
+                })
+              )
+  }
+
+  login() {
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+              .pipe(
+                tap( auth => this._auth = auth ), // Envia los datos a la ruta indicada
+                tap( auth => localStorage.setItem( 'id', auth.id ) )
+                );
+  }
+}
